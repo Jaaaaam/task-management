239dab37bfd87ab1b71fd270484332d07801d424
Jam <pjdsilvestre@tmj.ph>
2017-08-09 14:11:46 +0800

    initial commit. set up database

---
+++ app/Task.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+  public function users() {
+    return $this->belongsToMany('App\User', 'user_tasks');
+  }
+}
---
+++ database/migrations/2017_08_09_013415_create_table_events.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('place');
+            $table->date('date');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
